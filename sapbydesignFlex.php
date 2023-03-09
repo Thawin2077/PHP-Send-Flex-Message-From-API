449339ddb7a93ffec3edad8ead428f90ee07d916
Thawin Julprasitpong <123452161+Thawin2077@users.noreply.github.com>
2023-03-09 11:29:06 +0700

    Add Array
    add array $userIds and $tokens id to send flex multiple user

--- sapbydesignFlex.php
+++ sapbydesignFlex.php
@@ -1,4 +1,16 @@
 <?php
+
+$accessTokens = array(
+    'X60pZY7wrDmox5r3KAmID7Cfc6pIXAjIiFYq6WbAS/8jHngD92fzTUqE4LqUot46NhC72PFCLQv8rcWYz4fO4eBICY/XgHriUsi9kT9e9gvr9ezXr6trVwPGNvLeOJprq/QDQK4FQjHdqOk5c8MS4QdB04t89/1O/w1cDnyilFU=',
+    'Fvz1VqPIqZjAFlZuM+m8G79PJxLndN014t2SfCtoTr6a6DMzJUXt4GHp6SB31xN8tVQLn/BreKrat2DNtWtHMRPNhdnmIqjKZjFzlSkP71NsFnOqy+qW/sl3V1ZxnKNpwHr7qh3Apw3r0FvDqjS+kwdB04t89/1O/w1cDnyilFU=',
+    'pgvSNyyx0qI2BJfO5Flq0dphv47A3ZystqmSjJmbF5JB3CiFwfkPvW21dOvvsaecp8L6UG/KpB52DPV3/lF5Hivf8O7fsL1dZjaXmaMNEW1ud0kX4z+BeXjk56SvvCogdZqNP3u7p47l69ich41SJwdB04t89/1O/w1cDnyilFU=',
+);
+$userIds = array(
+    'Ubc3a95ba6527385cc8c2212c42ad5090',
+    'U4072e7dae4fe9428181793dc397a3ec3',
+    'U04edb82fd90e4d5c8f79424a1848bbfe',
+);
+
 // set API endpoint and query parameters
 $url = 'https://my358611.sapbydesign.com/sap/byd/odata/ana_businessanalytics_analytics.svc/RPZD606136405415E98FE8C0CQueryResults';
 $params = array(
@@ -162,57 +174,70 @@ foreach ($data['d']['results'] as $result) {
                     'contents' => $flexData
                 )
             ));
-// set LINE Messaging API endpoint
-$url = 'https://api.line.me/v2/bot/message/push';
 
-// set LINE Messaging API access token and user ID
-$accessToken = 'X60pZY7wrDmox5r3KAmID7Cfc6pIXAjIiFYq6WbAS/8jHngD92fzTUqE4LqUot46NhC72PFCLQv8rcWYz4fO4eBICY/XgHriUsi9kT9e9gvr9ezXr6trVwPGNvLeOJprq/QDQK4FQjHdqOk5c8MS4QdB04t89/1O/w1cDnyilFU=';
-$userId = 'Ubc3a95ba6527385cc8c2212c42ad5090';
+// loop through each access token and send the Flex message to each user
+foreach ($accessTokens as $accessToken) {
+    foreach ($userIds as $userId) {
+        // set API endpoint and query parameters
+        $url = "https://api.line.me/v2/bot/message/push";
+        $headers = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer '.$accessToken,
+        );
 
-// create HTTP headers for LINE Messaging API request
-$headers = array(
-    'Content-Type: application/json',
-    'Authorization: Bearer '.$accessToken,
-);
+        // create Flex message
+        $message = array(
+            'type' => 'flex',
+            'altText' => 'Inventory Data',
+            'contents' => array(
+                'type' => 'carousel',
+                'contents' => $flexData,
+            ),
+        );
 
-// create LINE Messaging API message payload
-$message = array(
-    'type' => 'flex',
-    'altText' => 'Inventory Data',
-    'contents' => array(
-        'type' => 'carousel',
-        'contents' => $flexData,
-    ),
-);
+        // create JSON data for API request body
+        $data = array(
+            'to' => $userId,
+            'messages' => array($message),
+        );
+        $postData = json_encode($data);
 
-// create LINE Messaging API request payload
-$data = array(
-    'to' => $userId,
-    'messages' => array($message),
-);
+        // initialize curl session
+        $ch = curl_init();
 
-// convert payload to JSON format
-$jsonData = json_encode($data);
+        // set curl options
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// initialize curl session
-$ch = curl_init();
+        // execute curl request and get response data
+        $response = curl_exec($ch);
 
-// set curl options
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // check if there was an error with the request
+        if ($response === false) {
+            echo 'Curl error: '.curl_error($ch);
+            exit;
+        }
 
-// execute curl request and get response data
-$response = curl_exec($ch);
+        // decode JSON response data
+        $responseData = json_decode($response, true);
 
-// check if there was an error with the request
-if ($response === false) {
-    echo 'Curl error: '.curl_error($ch);
-    exit;
+        // check if there was an error with the JSON decoding
+        if (is_null($responseData)) {
+            echo 'JSON error: '.json_last_error_msg();
+            exit;
+        }
+
+        // check if there was an error with the API request
+        if (isset($responseData['message'])) {
+            echo 'API error: '.$responseData['message'];
+            exit;
+        }
+
+        // close curl session
+        curl_close($ch);
+    }
 }
-
-// close curl session
-curl_close($ch);
  ?> 
